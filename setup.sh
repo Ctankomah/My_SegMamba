222a5d517c7dece72bcf9ddf54830eb84c8b16df
Claudia Takyi Ankomah <100475478+Ctankomah@users.noreply.github.com>
2025-05-14 19:44:26 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 # Install causal-conv1d
 cd causal-conv1d
-python setup.py install
+python setup.py install > /dev/null 2>&1
 cd ..
 
 # Install mamba
 cd mamba
-python setup.py install
+python setup.py install > /dev/null 2>&1
 cd ..
